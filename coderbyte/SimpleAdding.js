eb9bffd623bf1a6b6232baafb859273e5b1a34f7
ipung anwar <ipung.anwar1@gmail.com>
2018-10-24 22:52:44 +0700

    SimpleAdding

---
+++ coderbyte/SimpleAdding.js
@@ -0,0 +1,14 @@
+function SimpleAdding(num) { 
+
+  let sum = 0
+  
+  for (i = 0; i <= num; i++){
+    sum += i
+  }
+  
+  return sum
+         
+}
+   
+// keep this function call here 
+console.log(SimpleAdding(8))
